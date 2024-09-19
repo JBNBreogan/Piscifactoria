d8fd75280b7e94ef0e5d8bef464e3a0cdc1a30ea
crisjuvino <cristianjuvinosoto@gmail.com>
2024-09-19 17:45:57 +0200

    Prueba commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!ftu7ahjjjaewfhjahjafdhja");
+        System.out.println("Hola");
     }
 }
