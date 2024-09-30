4da30b494204cb50309e80143b418bd120e68dbe
Breogan <breogangg@gmail.com>
2024-09-30 19:03:44 +0200

    se me olvido el bagre

--- src/peces/Double/BagreDeCanal.java
+++ src/peces/Double/BagreDeCanal.java
@@ -6,22 +6,8 @@ import propiedades.AlmacenPropiedades;
 public class BagreDeCanal extends Pez{
     
     public BagreDeCanal(char Sexo){
-        this.Nombre = AlmacenPropiedades.BAGRE_CANAL.getNombre();
-        this.Cientifico = AlmacenPropiedades.BAGRE_CANAL.getCientifico();
-        this.Ciclo = AlmacenPropiedades.BAGRE_CANAL.getCiclo();
-        this.Coste = AlmacenPropiedades.BAGRE_CANAL.getCoste();
-        this.Huevos = AlmacenPropiedades.BAGRE_CANAL.getHuevos();
-        this.Madurez = AlmacenPropiedades.BAGRE_CANAL.getMadurez();
-        this.Monedas = AlmacenPropiedades.BAGRE_CANAL.getMonedas();
-        this.Optimo = AlmacenPropiedades.BAGRE_CANAL.getOptimo();
-        this.Piscifactoria = AlmacenPropiedades.BAGRE_CANAL.getPiscifactoria();
-        this.Propiedadas = AlmacenPropiedades.BAGRE_CANAL.getPropiedades();
-        this.Tipo = AlmacenPropiedades.BAGRE_CANAL.getTipo();
+        super(AlmacenPropiedades.BAGRE_CANAL);
         this.Sexo = Sexo;
-        this.Fertil = false;
-        this.Vivo = true;
-        this.Edad = 0;
     }
-
     //TODO todo el pez
 }
