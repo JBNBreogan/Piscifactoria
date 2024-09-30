66ebf6a39aac155b383819ff05c91c5174e23c28
Cristian Juvino Soto (Alumno de 2º Desenvolvemento de aplicacións multiplataforma) <cristianjuvinosoto@gmail.com>
2024-09-30 17:36:01 +0200

    Commit 2 Tanque

--- src/Tanque/Tanque.java
+++ src/Tanque/Tanque.java
@@ -5,27 +5,96 @@ import java.util.ArrayList;
 public class Tanque {
 
     private int numTanque;
+    private ArrayList<Pez> peces;
+    private int maxPeces;
     
-
     public void showStatus(){
         System.out.println("============Tanque "+ numTanque + "============");
-        System.out.println("Ocupación: " + pecesEnTanque +" / "+ maxPeces + " (" + porcentajeOcupacion + "%)");
-        System.out.println("Peces vivos: " + pecesVivos +" / "+pecesEnTanque + " (" + porcentajeVivos + "%)" );
-        System.out.println("Peces alimentados: " + pecesAlimentados +" / "+ pecesVivos + " (" + porcentajeAlimentados + "%)");
-        System.out.println("Peces adultos: " + pecesAdultos +" / "+ pecesVivos + " (" + porcentajeAlimentados + "%)");
-        System.out.println(pecesHembra+" / "+pecesMacho + "H/M");
-        System.out.println("Fértiles: " + pecesFertiles+" / "+pecesVivos);
+        System.out.println("Ocupación: " + pecesEnTanque() +" / "+ maxPeces + " (" + porcentajeOcupacion + "%)");
+        System.out.println("Peces vivos: " + pecesVivos(peces) +" / "+pecesEnTanque() + " (" + porcentajeVivos + "%)" );
+        System.out.println("Peces alimentados: " + pecesAlimentados(peces) +" / "+ pecesVivos(peces) + " (" + porcentajeAlimentados + "%)");
+        System.out.println("Peces adultos: " + pecesAdultos(peces) +" / "+ pecesVivos(peces) + " (" + porcentajeAlimentados + "%)");
+        System.out.println(pecesHembra(peces)+" / "+pecesMacho(peces) + "H/M");
+        System.out.println("Fértiles: " + pecesFertiles(peces)+" / "+pecesVivos(peces));
     }
     
-    public void showFishStatus(Pez pez){
-        pez.showStatus();
+    public void showFishStatus(ArrayList<Pez> peces){
+        for (Pez pez : peces) {
+            pez.showStatus();
+        }
     }
 
     public void showCapacity(Piscifactoria piscifactoria){
         System.out.println("Tanque " + numTanque +" de la piscifactoria "+ piscifactoria.getNum() + "al " + porcentajeOcupacion + "% de capacidad [" + pecesEnTanque +"/"+ maxPeces + "].");
     }
 
-    public void nextDay(){
-        //Llama al nextDay de todos los peces el tanque
+    public void nextDay(ArrayList<Pez> peces){
+        for (Pez pez : peces) {
+            pez.nextDay();
+        }
+    }
+
+    public static int pecesEnTanque(){
+        peces.lenght();
+    }
+
+    public static int pecesVivos(ArrayList<Pez> peces){
+        int contadorPecesVivos=0;
+        for (Pez pez : peces) {
+            if(pez.isVivo()){
+                contadorPecesVivos+=1;
+            }
+        }
+        return contadorPecesVivos;
+    }
+
+    public static int pecesAlimentados(ArrayList<Pez> peces){
+        int contadorPecesAlimentados=0;
+        for (Pez pez : peces) {
+            if(pez.isAlimentado()){
+                contadorPecesAlimentados+=1;
+            }
+        }
+        return contadorPecesAlimentados;
+    }
+
+    public static int pecesAdultos(ArrayList<Pez> peces){
+        int contadorPecesAdultos=0;
+        for (Pez pez : peces) {
+            if(pez.isAdulto()){
+                contadorPecesAdultos+=1;
+            }
+        }
+        return contadorPecesAdultos;
+    }
+
+    public static int pecesHembra(ArrayList<Pez> peces){
+        int contadorPecesHembra=0;
+        for (Pez pez : peces) {
+            if(pez.isHembra()){
+                contadorPecesHembra+=1;
+            }
+        }
+        return contadorPecesHembra;
+    }
+
+    public static int pecesMacho(ArrayList<Pez> peces){
+        int contadorPecesMacho=0;
+        for (Pez pez : peces) {
+            if(!pez.isHembra()){
+                contadorPecesMacho+=1;
+            }
+        }
+        return contadorPecesMacho;
+    }
+
+    public static int pecesFertiles(ArrayList<Pez> peces){
+        int contadorPecesFertiles=0;
+        for (Pez pez : peces) {
+            if(pez.isFertil()){
+                contadorPecesFertiles+=1;
+            }
+        }
+        return contadorPecesFertiles;
     }
 }
