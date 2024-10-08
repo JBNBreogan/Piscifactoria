a3158b7208fcb8c8902d3e7ac51c214f6fba8323
Cristian Juvino Soto (Alumno de 2º Desenvolvemento de aplicacións multiplataforma) <cristianjuvinosoto@gmail.com>
2024-10-08 17:16:24 +0200

    Avances addFood

--- src/Simulador.java
+++ src/Simulador.java
@@ -2,8 +2,10 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import Comun.Monedero;
 import Tanque.Tanque;
 import helpers.MenuHelper;
+import propiedades.CriaTipo;
 
 public class Simulador {
     private static int dias = 0;
@@ -16,7 +18,8 @@ public class Simulador {
         nombreempresa = sc.nextLine();
         System.out.println("Nombre de la piscifactoria");
         String npisc= sc.nextLine();
-        piscifactorias.add(new Piscifactoria(npisc));
+        piscifactorias.add(new Piscifactoria(npisc,CriaTipo.RIO));
+        Monedero.getInstance().setMonedas(100);
     }
 
     public static void menu(){
@@ -119,6 +122,79 @@ public class Simulador {
         System.out.println(pecesVendidos + " peces vendidos por un total de "+monedasObtenidas+ " monedas");
     }
 
+    public static void addFood(){
+        Piscifactoria pisc = piscifactorias.get(selectPisc());
+        //Falta implementar almacen central
+        int opcion;
+        String tipoComidaEleg="";
+        do {
+            System.out.println("Que tipo de comida quieres añadir?");
+            MenuHelper.mostrarMenu(new String[]{"Animal",
+                                                "Vegetal"},
+                                                false);
+            opcion=sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    tipoComidaEleg="Animal";
+                    break;
+                case 2:
+                    tipoComidaEleg="Vegetal";
+                    break;
+                default:
+                    System.out.println("Esa opcion no esta disponible, eliga entre 1 o 2");
+                    break;
+            }
+        } while (opcion!= 1 || opcion!=2);
+        
+        int opCant;
+        do {
+            System.out.println("Cuanta comida quieres añadir?");
+            MenuHelper.mostrarMenu(new String[]{"5",
+                                                "10",
+                                                "25",
+                                                "llenar"},
+                                                false);
+
+            opCant=sc.nextInt();
+            switch (opCant) {
+                case 1:
+                    if (tipoComidaEleg=="Animal") {
+                        //Falta settter para poder añadir comida en todas las opciones
+                    }else if (tipoComidaEleg=="Vegetal") {
+                        
+                    }
+                    break;
+                case 2:
+                if (tipoComidaEleg=="Animal") {
+                    //Falta settter para poder añadir comida en todas las opciones
+                }else if (tipoComidaEleg=="Vegetal") {
+                    
+                }
+                    break;
+                case 3:
+                if (tipoComidaEleg=="Animal") {
+                    //Falta settter para poder añadir comida en todas las opciones
+                }else if (tipoComidaEleg=="Vegetal") {
+                    
+                }
+                    break;
+                case 4:
+                if (tipoComidaEleg=="Animal") {
+                    //Falta settter para poder añadir comida en todas las opciones
+                }else if (tipoComidaEleg=="Vegetal") {
+                    
+                }
+                    break;
+                default:
+                    break;
+            }
+        } while (opCant!=1 || opCant!=2 || opCant!=3 || opCant!=4);
+
+        
+
+        
+
+    }
 
 
 
@@ -145,7 +221,7 @@ public class Simulador {
     public static int pecesTotalesEnPisc(){
         int pecesTotal=0;
         for (Piscifactoria piscifactoria : piscifactorias) {
-            pecesTotal += piscifactoria.pecesEnPisicfactoria();
+            pecesTotal += piscifactoria.pecesEnPiscifactoria();
         }
         return pecesTotal;
     }
@@ -156,6 +232,29 @@ public class Simulador {
             espacioTotal += piscifactoria.pecesMaxPiscifactoria();
         }
         return espacioTotal;
+    }
+
+    public static void añadirComidaVegetal(){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        menu();
+        init();
     }
 }
 
