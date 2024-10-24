565a704e4529044d85606da65789f2afff27926b
Nicolás Iglesias Solla (Alumno de 1º Desenvolvemento de aplicacións multiplataforma) <nicolaigl72@gmail.com>
2024-10-24 18:50:38 +0200

    repartir almacén central

--- src/Comun/AlmacenCentral.java
+++ src/Comun/AlmacenCentral.java
@@ -66,20 +66,52 @@ public class AlmacenCentral {
 
 
     public void repartir(ArrayList<Piscifactoria> piscifactorias){
-        
+        boolean todasllenas = false;
         int comidaanimalarepartir=comidaAnimal/piscifactorias.size();
         int comidavegetalarepartir=comidaVegetal/piscifactorias.size();
-      
+
         for (Piscifactoria piscifactoria : piscifactorias) {
-            piscifactoria.addFood(comidaanimalarepartir, "Animal");
-            piscifactoria.addFood(comidavegetalarepartir, "Vegetal");
+            if(piscifactoria.comidaAnimalLlena() && piscifactoria.comidaVegetalLlena()){
+                todasllenas = true;
+            } else {
+                todasllenas = false;
+                break;
+            }
         }
+        if(!todasllenas || !(comidaAnimal == 0 && comidaVegetal == 0)){
+            for (Piscifactoria piscifactoria : piscifactorias) {
+                if(piscifactoria.getMaxComidaAnimal()>comidaanimalarepartir+piscifactoria.getComidaAnimal()){
+                   
+                    int restar = (piscifactoria.getMaxComidaAnimal()-piscifactoria.getComidaAnimal());
+                    if(restar>comidaanimalarepartir){
+                        comidaAnimal=-comidaanimalarepartir;
+                        piscifactoria.addFood(comidaanimalarepartir, "Animal");
+                    } else{
+                        comidaAnimal=-restar;
+                        piscifactoria.addFood(restar, "Animal");
+                    }
+                }
+                
+                if(piscifactoria.getMaxComidaVegetal()>comidaanimalarepartir+piscifactoria.getComidaVegetal()){
+                    int restar = (piscifactoria.getMaxComidaVegetal()-piscifactoria.getComidaVegetal());
+                    if(restar>comidavegetalarepartir){
+                        comidaVegetal=-comidavegetalarepartir;
+                        piscifactoria.addFood(comidavegetalarepartir, "Vegetal");
+                    } else{
+                        comidaVegetal=-restar;
+                        piscifactoria.addFood(restar, "Vegetal");
+                    }
+
+                }
+                repartir(piscifactorias);
+            }
+        }
+        
     }
     
     public void aumentarCapacidad(int capacidad){
         this.capacidadcomidaanimal+=capacidad;
         this.capacidadcomidavegetal+=capacidad;
-    }
 
-    
+    }
 }
