b5f53fa31ac3a3f3e7dd30bd8b10a8d68222578b
Cristian Juvino Soto (Alumno de 2º Desenvolvemento de aplicacións multiplataforma) <cristianjuvinosoto@gmail.com>
2025-02-04 16:32:33 +0100

    conexion base de datos

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -9,15 +9,15 @@ public class Conexion {
     /**Objeto para la conexion a la base de datos*/
     public static Connection conn;
     /**Nombre de usuario de la base de datos */
-    private static String USERNAME="";
+    private static String USERNAME="cjuvino";
     /**Contraseña del usuario para la base de datos */
-    private static String PASSWORD="";
+    private static String PASSWORD="abc123.";
     /**Direccion ip del servidor donde esta la base de datos */
-    private static String SERVER="";
+    private static String SERVER="213.32.47.55";
     /**Puerto de la base de datos */
-    private static String PORT_NUMBER="";
+    private static String PORT_NUMBER="3306";
     /**Nombre de la base de datos */
-    private static String DATABASE="";
+    private static String DATABASE="pescaditos";
 
     /**
      * Constructor vacio de la clase conexion
